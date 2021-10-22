c4c8e3abe4eecec7b42d75539dd04a8392c68111
in uh, lee <boutime2017@gmail.com>
2021-10-22 10:51:14 +0900

    filter Operator

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
-globalThis.XMLHttpRequest = require("xhr2");
+import { filter, Observable } from "rxjs";
 
-import { forkJoin, Observable } from "rxjs";
+interface NewsItem {
+  category: "Business" | "Sports";
+  content: string;
+}
 
-const a$ = new Observable((subscriber) => {
-  const timeoutId = setTimeout(() => {
-    subscriber.next("a");
-    subscriber.complete();
-  }, 5000);
-  return () => {
-    clearTimeout(timeoutId);
-    console.log("a teardown");
-  };
-});
-
-const b$ = new Observable((subscriber) => {
-  const timeoutId = setTimeout(() => {
-    subscriber.error("에러");
+const newsFeed$ = new Observable<NewsItem>((subscriber) => {
+  setTimeout(() => {
+    subscriber.next({ category: "Business", content: "a" });
+  }, 1000);
+  setTimeout(() => {
+    subscriber.next({ category: "Sports", content: "b" });
   }, 3000);
-  // b$가 에러가 나면, 남은 2초 동안 a$를 기다릴 이유가 없다.
-  // 따라서 b$의 error가 forkJoin의 error로 넘어간 뒤,
-  // a의 teardown과 b의 teardown이 순서대로 실행된다.
-  // 역시, Promise.all과 닮았다.
-  return () => {
-    clearTimeout(timeoutId);
-    console.log("b teardown");
-  };
+  setTimeout(() => {
+    subscriber.next({ category: "Business", content: "c" });
+  }, 4000);
+  setTimeout(() => {
+    subscriber.next({ category: "Sports", content: "d" });
+  }, 6000);
+  setTimeout(() => {
+    subscriber.next({ category: "Business", content: "e" });
+  }, 7000);
 });
 
-forkJoin([a$, b$]).subscribe({
-  next: (values) => console.log(values),
-  error: (e) => console.error("Error : ", e),
+const sportsNewsFeed$ = newsFeed$.pipe(
+  filter((item) => item.category === "Sports")
+);
+
+sportsNewsFeed$.subscribe({
+  next: (item: NewsItem) => console.log(item),
 });
