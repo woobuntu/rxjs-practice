3f46a9c70cce734e58970b4297f3f5d43489598f
in uh, lee <boutime2017@gmail.com>
2021-10-24 16:08:01 +0900

    concatMap Operator - dynamic example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import { concatMap, Observable, of } from "rxjs";
+import { catchError, concatMap, of } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-const source$ = new Observable((subscriber) => {
-  setTimeout(() => {
-    subscriber.next("a");
-  }, 2000);
-  setTimeout(() => {
-    subscriber.next("b");
-  }, 5000);
-});
+globalThis.XMLHttpRequest = require("xhr2");
 
-source$
-  .pipe(concatMap((value) => of(1, 2)))
-  .subscribe({ next: (value) => console.log(value) });
+of("none", "food")
+  .pipe(
+    concatMap((value) =>
+      ajax(`https://random-data-api.com/api/${value}/random_${value}`).pipe(
+        catchError((error) => of(`Error : ${error.message}`))
+      )
+    )
+  )
+  .subscribe({
+    next: (value) => console.log(value),
+    // error: (e) => console.error("error : ", e.message),
+    // 위에서 catchError로 핸들링하기 때문에 사실상 무의미
+    complete: () => console.log("complete"),
+  });
