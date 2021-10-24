fd4bb12f0445e7a187e0bd32ee6dbdbd734fb259
in uh, lee <boutime2017@gmail.com>
2021-10-24 20:42:51 +0900

    async function과 Observable 조합 실험

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { catchError, concatMap, of } from "rxjs";
-import { ajax } from "rxjs/ajax";
+import { from } from "rxjs";
+import axios from "axios";
 
-globalThis.XMLHttpRequest = require("xhr2");
+async function fetch(value: string) {
+  try {
+    const { data } = await axios({
+      url: `https://random-data-api.com/api/${value}/random_${value}`,
+    });
+    return data; // next로 emit될 값
+  } catch (error: any) {
+    return error.message; // error로 emit될 값
+  }
+}
 
-of("none", "food")
-  .pipe(
-    concatMap((value) =>
-      ajax(`https://random-data-api.com/api/${value}/random_${value}`).pipe(
-        catchError((error) => of(`Error : ${error.message}`))
-      )
-    )
-  )
-  .subscribe({
-    next: (value) => console.log(value),
-    // error: (e) => console.error("error : ", e.message),
-    // 위에서 catchError로 핸들링하기 때문에 사실상 무의미
-    complete: () => console.log("complete"),
-  });
+from(fetch("none")).subscribe({
+  next: (value) => console.log(value),
+  error: (error) => console.log("Error : ", error),
+  complete: () => console.log("complete"),
+});
